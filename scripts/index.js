bfaad6acaca50d75ec8ce8763c17bce1963cda7c
nelimalu <lukaj2501@gmail.com>
2024-03-14 23:31:02 -0400

    added panels

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,33 @@
-var flipword_interval = 10;
-var flip_words = [
-    "STUDENTS",
-    "LEADERS",
-    "THINKERS",
-    "INNOVATORS",
-    "OUR FUTURE",
-    "ANYBODY",
-    "YOU"
-];
-var counter = 1;
+function distance(x1, x2, y1, y2) {
+  return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+}
+
+function squish(value, in_min, in_max, out_min, out_max) {
+  let percentage = (value - in_min) / (in_max - in_min);
+  return percentage * (out_max - out_min) + out_min;  
+}
 
 
+window.onload = function() {
+  document.addEventListener('mousemove', (event) => {
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    
-    /*
-    let flipword = document.getElementById("flip-word");
-    flipword.style.animation = `skew ${flipword_interval}s ease infinite`;
+    let panels = document.getElementsByClassName("panel");
 
-    function change_word() {
-        flipword.innerHTML = flip_words[counter];
-        counter++;
-        counter %= flip_words.length;
+    for (let panel of panels) {
+      let centerX = panel.offsetLeft + panel.offsetWidth / 2;
+      let centerY = panel.offsetTop + panel.offsetHeight / 2;
+
+      let xdist = event.x - centerX;
+      let ydist = event.y - centerY;
+
+      let xangle = squish(xdist, -panel.offsetWidth / 2, panel.offsetWidth / 2, -8, 8);
+      let yangle = squish(ydist, -panel.offsetHeight / 2, panel.offsetHeight / 2, 2, -2);
+      console.log(xangle, yangle)
+
+      panel.style.setProperty('--y-angle', `${xangle}deg`);
+      panel.style.setProperty('--x-angle', `${yangle}deg`);
+
     }
 
-    setTimeout(() => {
-        change_word();
-        setInterval(() => { 
-            change_word();
-        }, flipword_interval * 1000);
-    }, flipword_interval * 500)
-    */
-   
-
-});
+  });
+}
